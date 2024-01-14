3b7beda5795e1a4f331c4da5d2ae0cdb0d97df89
minhbui <minh.bn213997@sis.hust.edu.vn>
2024-01-14 09:58:07 +0700

    update validate categoryid

--- Finance.Models/Models/Income.cs
+++ Finance.Models/Models/Income.cs
@@ -13,7 +13,7 @@ namespace Finance.Models.Models
         public int Id { get; set; }
         [Required]
         public DateTime Time { get; set; }
-
+        [Required]
         public int IncomeCategoryId { get; set; }
 
         [ForeignKey("IncomeCategoryId")]
